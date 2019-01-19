9c0befe032fe4637d5a9290bebd5250edf8b0518
Vbitz <jscarsbrook@gmail.com>
2019-01-19 22:36:13 +1300

    Added createCreature stub to d20/fifth/registry/creature_registry

--- src/d20/fifth/registry/creature_registry.ts
+++ src/d20/fifth/registry/creature_registry.ts
@@ -5,6 +5,9 @@ export type CreateCreatureCallback = (ctx: Core.Context) => Core.Entity;
 export const registerCreature: (name: string, cb: CreateCreatureCallback) =>
     void = 'd20.creature_registry.register' as Core.EventDeclaration;
 
+export const createCreature: (name: string) => Core.Entity =
+    'd20.creature_registry.create' as Core.EventDeclaration;
+
 export const createCreatureRegistry: () => void =
     'd20.creature_registry.create' as Core.EventDeclaration;
 
@@ -17,12 +20,17 @@ class CreatureRegistry {
 export class CreatureRegistryModule extends Core.Module {
   async onCreate(ctx: Core.Context) {
     // TODO(joshua): Why is this a root handler and not a post-init callback. It
-    // could even be a private member of this class.
+    // could even be a private member of this class. That would also make
+    // serialization easier since Modules could be saved.
     ctx.registerRootHandler(createCreatureRegistry, async (ctx) => {
       const creatureRegistry = new CreatureRegistry();
 
       ctx.registerRootHandler(registerCreature, async (ctx, name, cb) => {
         creatureRegistry.register(name, cb);
+      });
+
+      ctx.registerRootHandler(createCreature, async (ctx, name) => {
+        throw new Error('Not Implemented');
       });
     });
   }
