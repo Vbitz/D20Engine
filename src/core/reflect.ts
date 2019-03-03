2ff4e87b81014181229cf5c14f3b88656974daf8
Joshua Scarsbrook <jscarsbrook@gmail.com>
2019-03-03 18:59:16 +1300

    Documented reflect.ts. Thanks Daniel.

--- src/core/reflect.ts
+++ src/core/reflect.ts
@@ -48,6 +48,10 @@ export interface NullableField extends BaseObjectField {
 export type ObjectField =
     BasicField|StatefulObjectField|DiceSpecificationField|DiceResultField|EnumField|ArrayField|NullableField;
 
+/**
+ * The StatefulObject's constructor is casted to this type to add the metadata
+ * as a static field.
+ */
 interface StatefulObjectMetadata {
   __fields?: Core.Common.Bag<ObjectField>;
 }
@@ -67,6 +71,8 @@ function getRootPath(dirname: string): string {
 }
 
 function getName(name: ts.PropertyName|undefined): string|undefined {
+  // TODO(joshua): This is pretty fragile right now. If code uses more obscure
+  // field naming techniques then this will break.
   if (name === undefined) {
     return undefined;
   } else if (ts.isIdentifier(name)) {
@@ -77,23 +83,27 @@ function getName(name: ts.PropertyName|undefined): string|undefined {
 }
 
 class ReflectionMetadata {
-  private basePath: string;
   private config: ts.CompilerOptions;
+
   private program: ts.Program;
+
   private typeChecker: ts.TypeChecker;
 
   constructor() {
+    // I've added console logging to this to get a indication of how much time
+    // this taking. The impact should be pretty minimal.
     console.log('Start Init ReflectionMetadata');
 
     const basePath = getRootPath(__dirname);
 
-    this.basePath = basePath;
-
+    // This code assumes a normal TypeScript project layout and no sub-projects.
     const tsConfigFilename = path.join(basePath, 'tsconfig.json');
 
     const config = ts.readConfigFile(
         tsConfigFilename, (path) => readFileSync(path, 'utf8'));
 
+    // I'm not sure how this is supposed to be done but it works. I'd rather use
+    // async filesystem methods but TypeScript normally uses the sync versions.
     const parseConfigHost: ts.ParseConfigHost = ts.sys;
 
     const parsedConfig =
@@ -101,24 +111,41 @@ class ReflectionMetadata {
 
     this.config = parsedConfig.options;
 
+    // Create a program from the parsed configuration. This should match the
+    // configuration used to build the engine in the first place.
     const program = ts.createProgram({
       rootNames: parsedConfig.fileNames,
       options: parsedConfig.options,
       projectReferences: parsedConfig.projectReferences
     });
 
+    // Since this is the same configuration it's assumed any errors would
+    // prevent this from running in the first place. That's why diagnostics are
+    // not handled.
+
     this.program = program;
+
     this.typeChecker = program.getTypeChecker();
 
     console.log('Finish Init ReflectionMetadata');
   }
 
+  /**
+   * Embed metadata for all exported classes deriving from StatefulObjects as a
+   * static field on the class. This is intended to be used for user-interface
+   * and save/load systems.
+   * This should appear as the last line in the file so all exports are
+   * initialized.
+   * @param fileModule The `module` global from the file. This is used to
+   * determine the filename and list of exported symbols.
+   */
   embed(fileModule: NodeModule) {
     const javascriptFilename = fileModule.filename;
 
     const sourceFile = this.getTypescriptSourceFile(javascriptFilename);
 
     if (sourceFile === undefined) {
+      // This could happen if the file in the sub-module is wrong.
       throw new Error('Could not get file AST');
     }
 
@@ -126,47 +153,67 @@ class ReflectionMetadata {
   }
 
   private walkSourceTree(fileModule: NodeModule, node: ts.Node) {
+    // This method only looks for class declarations
+
     if (ts.isClassDeclaration(node)) {
       if (node.name === undefined) {
         return;
       }
 
+      // This approach only works for exported classes since otherwise there's
+      // no easy way to automatically enumerate class declarations. I also don't
+      // assume there are any major use cases that would benefit from adding
+      // metadata to non-exported classes.
       if (!this.isClassExported(node)) {
         return;
       }
+
+      // Attempt to find the runtime version of this class declaration.
 
       const className = node.name.getText(node.getSourceFile());
 
       const runtimeClass = fileModule.exports[className];
 
+      // Check using the prototype chain if the class inherits from
+      // StatefulObject. In practice this will only work as expected if there
+      // are no non-trivial classes in between this class and StatefulObject
+      // since no metadata is added for other classes in the chain.
+
       if (!this.isStatefulObject(runtimeClass)) {
         return;
       }
 
-      this.embedMetadata(fileModule.filename, className, node, runtimeClass);
+      this.embedMetadata(fileModule.filename, node, runtimeClass);
     }
 
     ts.forEachChild(node, this.walkSourceTree.bind(this, fileModule));
   }
 
   private embedMetadata(
-      filename: string, className: string, node: ts.ClassDeclaration,
+      filename: string, node: ts.ClassDeclaration,
       runtimeClass: Core.StatefulObject) {
     const fields: Core.Common.Bag<ObjectField> = {};
 
+    // TODO(joshua): Handle inheritance. Right now this will only extract
+    // members from the top level class declaration.
     for (const member of node.members) {
       const memberName = getName(member.name);
 
+      // Only add metadata for properties. Methods are not exported or otherwise
+      // exposed outside the declaration of a StatefulObject.
       if (!ts.isPropertyDeclaration(member) || memberName === undefined) {
         continue;
       }
 
+      // Skip private members.
       if (this.isMemberPrivate(member)) {
         continue;
       }
 
       let memberType: ts.Type|undefined = undefined;
 
+      // Get the type from either the explicit type or the inferred type in the
+      // initializer.
       if (member.type === undefined) {
         if (member.initializer !== undefined) {
           const initializerType =
@@ -188,6 +235,7 @@ class ReflectionMetadata {
       fields[memberName] = fieldDescription;
     }
 
+    // Set the metadata on the StatefulObject.
     this.setStatefulObjectMetadata(runtimeClass, fields);
   }
 
@@ -196,10 +244,16 @@ class ReflectionMetadata {
       memberType: ts.Type): ObjectField {
     if (memberType.flags & ts.TypeFlags.Enum
         || memberType.flags & ts.TypeFlags.EnumLiteral) {
+      // Enum is handled first as depending on the initializers enums could be
+      // other other types as well.
+
       return this.getEnumMetadataField(memberType);
     } else if (
         memberType.flags & ts.TypeFlags.String
         || memberType.flags & ts.TypeFlags.StringLiteral) {
+      // Due to the way types are determined literals are grouped together. This
+      // could be a limitation or source of bugs.
+
       return {type: 'string'};
     } else if (
         memberType.flags & ts.TypeFlags.Number
@@ -212,6 +266,7 @@ class ReflectionMetadata {
     } else if (memberType.flags & ts.TypeFlags.Object) {
       const objectType = memberType as ts.ObjectType;
 
+      // Try to determine if the object is an array by looking at the reference.
       if (this.isArrayCompileTime(memberType)) {
         if (!(objectType.objectFlags & ts.ObjectFlags.Reference)) {
           throw new Error('Array is not a reference type');
@@ -234,8 +289,14 @@ class ReflectionMetadata {
 
         return {type: 'array', valueType};
       } else if (this.isStatefulObjectCompileTime(memberType)) {
+        // StatefulObject has a more complicated implementation so it's split
+        // into it's own method.
+
         return this.getObjectMetadataField(memberType);
       } else if (this.isDiceResultsCompileTime(memberType)) {
+        // Both results and specification are complex primitives so need to be
+        // explicitly detected but are otherwise treated like strings.
+
         return {type: 'diceResult'};
       } else if (this.isDiceSpecCompileTime(memberType)) {
         return {type: 'diceSpecification'};
@@ -244,9 +305,8 @@ class ReflectionMetadata {
       }
     } else if (memberType.flags & ts.TypeFlags.UnionOrIntersection) {
       if (memberType.isUnion()) {
-        console.log('embedMetadataToField', filename, memberName, 'union');
-
-        console.log(memberType.types.length);
+        // Only 1 case where a union could be used to implement nullable types
+        // are implemented here.
 
         if (memberType.types.length !== 2) {
           throw new Error('Only unions with 2 types are supported (type|null)');
@@ -254,8 +314,9 @@ class ReflectionMetadata {
 
         const [type, nullType] = memberType.types;
 
-        if (nullType.getFlags() && ts.TypeFlags.Null
-            || nullType.getFlags() && ts.TypeFlags.Undefined) {
+        if (!(nullType.getFlags() && ts.TypeFlags.Null
+              || nullType.getFlags() && ts.TypeFlags.Undefined)) {
+          throw new Error('Second type is not null/undefined.');
         }
 
         const valueType =
@@ -268,6 +329,8 @@ class ReflectionMetadata {
 
         throw new Error('Intersection Types Not Implemented');
       } else {
+        // Here is will Entity/Component references will be implemented.
+
         throw new Error('Not Implemented');
       }
     } else {
@@ -279,11 +342,16 @@ class ReflectionMetadata {
     }
   }
 
-  private getEnumMetadataField(type: ts.Type): EnumField {
+  /**
+   * Get the declaration node using the symbol table. This will only work in
+   * trivial cases and is a simplification of how the language service goto
+   * declaration method works.
+   */
+  private getDeclarationFromSymbol(type: ts.Type): ts.Node {
     const symbol = type.getSymbol();
 
     if (symbol === undefined) {
-      throw new Error('Could not get symbol for enum');
+      throw new Error('Could not get symbol for type');
     }
 
     const declarations = symbol.getDeclarations();
@@ -292,8 +360,15 @@ class ReflectionMetadata {
       throw new Error('Could not get declaration from symbol');
     }
 
-    let enumNode = declarations[0];
+    return declarations[0];
+  }
 
+  private getEnumMetadataField(type: ts.Type): EnumField {
+    let enumNode = this.getDeclarationFromSymbol(type);
+
+    // If the enum only contains one member than the declaration node could be
+    // this member rather the enum. Fortunately getting the parent is safe in
+    // this case.
     if (ts.isEnumMember(enumNode)) {
       enumNode = enumNode.parent;
     }
@@ -305,6 +380,7 @@ class ReflectionMetadata {
 
     const values: Core.Common.Bag<string> = {};
 
+    // Iterate though the members of the enum.
     for (const member of enumNode.members) {
       if (member.initializer === undefined
           || !ts.isStringLiteral(member.initializer)) {
@@ -326,20 +402,14 @@ class ReflectionMetadata {
     return {type: 'enum', name: getName(enumNode.name) || 'unknown', values};
   }
 
+  /**
+   * StatefulObject metadata is implemented as a reference rather than the
+   * literal to avoid circular dependencies. The indentation is that
+   * StatefulObjects are centrally registered so the details can be looked up at
+   * runtime.
+   */
   private getObjectMetadataField(type: ts.Type): StatefulObjectField {
-    const symbol = type.getSymbol();
-
-    if (symbol === undefined) {
-      throw new Error('Could not get symbol for object');
-    }
-
-    const declarations = symbol.getDeclarations();
-
-    if (declarations === undefined || declarations.length !== 1) {
-      throw new Error('Could not get declaration from symbol');
-    }
-
-    const objectNode = declarations[0];
+    const objectNode = this.getDeclarationFromSymbol(type);
 
     if (!ts.isClassDeclaration(objectNode)) {
       throw new Error(`Node is no an class node: node.kind=${
@@ -353,6 +423,9 @@ class ReflectionMetadata {
     };
   }
 
+  /**
+   * Returns if the member contains a private modifier.
+   */
   private isMemberPrivate(member: ts.PropertyDeclaration) {
     if (member.modifiers === undefined) {
       return false;
@@ -367,6 +440,10 @@ class ReflectionMetadata {
     return false;
   }
 
+  // Detection of tags rather then walking the prototype chain is used for
+  // it's simplicity. For types controlled by D20Engine this is reliable and
+  // will only trigger false positives in abusive situations.
+
   private isStatefulObjectCompileTime(type: ts.Type) {
     return this.hasPropertyCompileTime(type, '__statefulObjectTag');
   }
@@ -379,6 +456,11 @@ class ReflectionMetadata {
     return this.hasPropertyCompileTime(type, '__diceSpecificationTag');
   }
 
+  /**
+   * As the internal implementation of arrays are not under the control of
+   * D20Engine detection is instead handled by looking for the declaration and
+   * checking that it's in the standard library.
+   */
   private isArrayCompileTime(type: ts.Type) {
     const symbol = type.getSymbol();
 
@@ -424,6 +506,9 @@ class ReflectionMetadata {
     return false;
   }
 
+  /**
+   * Look up the prototype chain to see if obj inherits from StatefulObject.
+   */
   // tslint:disable-next-line: no-any
   private isStatefulObject(obj: any): boolean {
     const superConstructor = Object.getPrototypeOf(obj);
@@ -437,6 +522,9 @@ class ReflectionMetadata {
     }
   }
 
+  /**
+   * Check if a class is exported by looking for an export keyword.
+   */
   private isClassExported(classDecl: ts.ClassDeclaration) {
     if (classDecl.modifiers === undefined) {
       return false;
@@ -462,6 +550,10 @@ class ReflectionMetadata {
     objMetadata.__fields = fields;
   }
 
+  /**
+   * Uses the configuration in tsconfig to attempt to resolve the location of
+   * the original source file from the built javascript file.
+   */
   private getTypescriptSourceFile(filename: string) {
     const relativeFilename = path.relative(this.config.outDir || '', filename);
     const typescriptFilename =
